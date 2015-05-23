3542cf11c2aa7bb90ae15f8fb8340980e3ab08cb
Oskar Gewalli <gewalli@gmail.com>
2015-05-23 20:49:11 +0200

    Generated code in order to have a mockup of svc

---
+++ CustomerService/Customer.cs
@@ -0,0 +1,141 @@
+﻿using System.Runtime.Serialization;
+namespace Customers
+{
+    [DataContract(Name = "Customer", Namespace = "http://schemas.datacontract.org/2004/07/Customers")]
+    public partial class Customer : object, IExtensibleDataObject
+    {
+        private ExtensionDataObject extensionDataField;
+
+        private int AccountNumberField;
+
+        private string AddressCityField;
+
+        private string AddressCountryField;
+
+        private string AddressStreetField;
+
+        private string FirstNameField;
+
+        private Customers.CustomerGender GenderField;
+
+        private string LastNameField;
+
+        private System.Uri PictureUriField;
+
+        public ExtensionDataObject ExtensionData
+        {
+            get
+            {
+                return this.extensionDataField;
+            }
+            set
+            {
+                this.extensionDataField = value;
+            }
+        }
+
+        [DataMember()]
+        public int AccountNumber
+        {
+            get
+            {
+                return this.AccountNumberField;
+            }
+            set
+            {
+                this.AccountNumberField = value;
+            }
+        }
+
+        [DataMember()]
+        public string AddressCity
+        {
+            get
+            {
+                return this.AddressCityField;
+            }
+            set
+            {
+                this.AddressCityField = value;
+            }
+        }
+
+        [DataMember()]
+        public string AddressCountry
+        {
+            get
+            {
+                return this.AddressCountryField;
+            }
+            set
+            {
+                this.AddressCountryField = value;
+            }
+        }
+
+        [DataMember()]
+        public string AddressStreet
+        {
+            get
+            {
+                return this.AddressStreetField;
+            }
+            set
+            {
+                this.AddressStreetField = value;
+            }
+        }
+
+        [DataMember()]
+        public string FirstName
+        {
+            get
+            {
+                return this.FirstNameField;
+            }
+            set
+            {
+                this.FirstNameField = value;
+            }
+        }
+
+        [DataMember()]
+        public CustomerGender Gender
+        {
+            get
+            {
+                return this.GenderField;
+            }
+            set
+            {
+                this.GenderField = value;
+            }
+        }
+
+        [DataMember()]
+        public string LastName
+        {
+            get
+            {
+                return this.LastNameField;
+            }
+            set
+            {
+                this.LastNameField = value;
+            }
+        }
+
+        [DataMember()]
+        public System.Uri PictureUri
+        {
+            get
+            {
+                return this.PictureUriField;
+            }
+            set
+            {
+                this.PictureUriField = value;
+            }
+        }
+    }
+}
---
+++ CustomerService/CustomerService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace Customers
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class CustomerService: ICustomerService
+    {
+        public Customer[] GetAllCustomers()
+        {
+            return new[] { new Customer { FirstName="Oskar", LastName="Gewalli" } };
+        }
+
+        public Customer GetCustomerByAccountNumber(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer[] GetCustomers(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveCustomer(Customer editedCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveCustomerLastName(string accountNumber, string newName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
