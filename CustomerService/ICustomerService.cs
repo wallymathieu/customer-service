785e3bff42a80db3080bd619a2b87553a1672245
Oskar Gewalli <gewalli@gmail.com>
2015-05-24 15:46:54 +0200

    Configuration in order for it to work with site

--- CustomerService/ICustomerService.cs
+++ CustomerService/ICustomerService.cs
@@ -10,25 +10,40 @@
 
 using System.ServiceModel;
 using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
 
 namespace Customers
 {
     [ServiceContract(Namespace = "http://www.galasoft.ch")]
     public interface ICustomerService
     {
-
+        [WebInvoke(Method = "GET",
+ BodyStyle = WebMessageBodyStyle.WrappedRequest,
+ ResponseFormat = WebMessageFormat.Xml)]
         [OperationContract(Action = "http://www.galasoft.ch/CustomerService/GetCustomers", ReplyAction = "http://www.galasoft.ch/CustomerService/GetCustomersResponse")]
         Customers.Customer[] GetCustomers(string lastName);
 
+        [WebInvoke(Method = "GET",
+BodyStyle = WebMessageBodyStyle.WrappedRequest,
+ResponseFormat = WebMessageFormat.Xml)]
         [OperationContract(Action = "http://www.galasoft.ch/CustomerService/GetCustomerByAccountNumber", ReplyAction = "http://www.galasoft.ch/CustomerService/GetCustomerByAccountNumberResponse")]
         Customers.Customer GetCustomerByAccountNumber(int accountNumber);
 
+        [WebInvoke(Method = "GET",
+ BodyStyle = WebMessageBodyStyle.WrappedRequest,
+ ResponseFormat = WebMessageFormat.Xml)]
         [OperationContract(Action = "http://www.galasoft.ch/CustomerService/GetAllCustomers", ReplyAction = "http://www.galasoft.ch/CustomerService/GetAllCustomersResponse")]
         Customers.Customer[] GetAllCustomers();
 
+        [WebInvoke(Method = "POST",
+BodyStyle = WebMessageBodyStyle.WrappedRequest,
+ResponseFormat = WebMessageFormat.Xml)]
         [OperationContract(Action = "http://www.galasoft.ch/CustomerService/SaveCustomer", ReplyAction = "http://www.galasoft.ch/CustomerService/SaveCustomerResponse")]
         bool SaveCustomer(Customers.Customer editedCustomer);
 
+        [WebInvoke(Method = "POST",
+BodyStyle = WebMessageBodyStyle.WrappedRequest,
+ResponseFormat = WebMessageFormat.Xml)]
         [OperationContract(Action = "http://www.galasoft.ch/CustomerService/SaveCustomerLastName", ReplyAction = "http://www.galasoft.ch/CustomerService/SaveCustomerLastNameResponse")]
         bool SaveCustomerLastName(string accountNumber, string newName);
     }
