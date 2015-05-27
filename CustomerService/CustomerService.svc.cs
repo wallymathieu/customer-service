e4ce7f53fe512591f20b7c7d65f7fc263ceb1603
Oskar Gewalli <gewalli@gmail.com>
2015-05-27 23:23:16 +0200

    Mocked more of the api

--- CustomerService/CustomerService.svc.cs
+++ CustomerService/CustomerService.svc.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
+using System.Linq;
 
 namespace Customers
 {
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-    public class CustomerService: ICustomerService
+    public class CustomerService : ICustomerService
     {
         public Customer[] GetAllCustomers()
         {
-            return new[] { new Customer { FirstName="Oskar", LastName="Gewalli" } };
+            return new[] {
+                new Customer { AccountNumber=1, FirstName = "Oskar", LastName = "Gewalli" },
+                new Customer { AccountNumber=2,FirstName = "Greta", LastName = "Skogsberg" },
+                new Customer { AccountNumber=3,FirstName = "Matthias", LastName = "Wallisson" },
+                new Customer { AccountNumber=4,FirstName = "Ada", LastName = "Lundborg" },
+                new Customer { AccountNumber=5,FirstName = "Daniel", LastName = "Örnvik" },
+                new Customer { AccountNumber=6,FirstName = "Johan", LastName = "Irisson" },
+                new Customer { AccountNumber=7,FirstName = "Edda", LastName = "Tyvinge" },
+            };
         }
 
         public Customer GetCustomerByAccountNumber(int accountNumber)
         {
-            throw new NotImplementedException();
+            return GetAllCustomers().SingleOrDefault(c => c.AccountNumber == accountNumber);
         }
 
         public Customer[] GetCustomers(string lastName)
         {
-            throw new NotImplementedException();
+            return GetAllCustomers().Where(c => c.LastName == lastName).ToArray();
         }
 
         public bool SaveCustomer(Customer editedCustomer)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public bool SaveCustomerLastName(string accountNumber, string newName)
         {
-            throw new NotImplementedException();
+            return false;
         }
     }
 }
