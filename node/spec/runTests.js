8d08cedae471c767fc007e6b3ea99f13db22afcd
Oskar Gewalli <oskar@gewalli.se>
2015-06-12 22:26:15 +0200

    No need for a runTests file

--- node/spec/runTests.js
+++
@@ -1,8 +0,0 @@
-///<reference path="../lib/config.ts"/>
-///<reference path="CustomerServiceTest.ts"/>
-///<reference path="../typings/requirejs/require.d.ts"/>
-'use strict';
-var CustomerServiceTest = require("./CustomerServiceTest");
-module.exports = {
-  tests: [CustomerServiceTest]
-};
