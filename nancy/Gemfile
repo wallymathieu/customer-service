c31b8a47b7bf7d3519dd466a478329a80ffe90fe
Oskar Gewalli <oskar@gewalli.se>
2015-06-28 10:28:35 +0200

    rakefiles

---
+++ nancy/Gemfile
@@ -0,0 +1,5 @@
+#ruby=2.2.0
+source 'http://rubygems.org'
+gem 'rake'
+gem 'albacore'
+gem 'nuget_helper', '~> 0.0.11'
---
+++ nancy/rakefile.rb
@@ -0,0 +1,28 @@
+require 'albacore'
+require 'nuget_helper'
+
+$dir = File.dirname(__FILE__)
+
+desc "Install missing NuGet packages."
+task :install_packages do
+  NugetHelper.exec("restore CustomerService.sln")
+end
+
+desc "build"
+build :build => [:install_packages] do |msb|
+  msb.prop :configuration, :Debug
+  msb.prop :platform, "x86"
+  msb.target = :Rebuild
+  msb.be_quiet
+  msb.nologo
+  msb.sln =File.join($dir, "CustomerService.sln")
+end
+
+task :default => ['build']
+
+desc "test using nunit console"
+test_runner :test => [:build] do |nunit|
+  nunit.exe = NugetHelper.nunit_path
+  files = Dir.glob(File.join($dir,"*Tests","bin","**","*Tests.dll")) 
+  nunit.files = files 
+end
