bcea5e5859de420fd983fe536c3ac85f81ac4279
Oskar Gewalli <gewalli@gmail.com>
2015-08-23 13:12:36 +0200

    using fsharpx.extras

--- suave/BillingService/CustomerInvoice.cs
+++ suave/BillingService/CustomerInvoice.cs
@@ -2,25 +2,25 @@
 using System.Text;
 using Customers;
 using Microsoft.FSharp.Core;
+using FSharpx;
 namespace BillingService
 {
-	public class GenerateCustomerInvoice
-	{
-		public string Create(Customer customer, string[] products)
-		{
-			var sb = new StringBuilder ();
-			sb.AppendLine(string.Format("Invoice for #{0}", customer.AccountNumber));
-			sb.AppendLine();
-			sb.AppendLine(string.Format("To:\t\t{0} {1}", customer.FirstName, customer.LastName));
-			sb.AppendLine(string.Format("Street:\t\t{0}",  customer.AddressStreet));
-			sb.AppendLine(string.Format("City:\t\t{0}",    customer.AddressCity));
-			sb.AppendLine(string.Format("Country:\t\t{0}", customer.AddressCountry));
-			if (customer.PictureUri != null)
-			{
-				sb.AppendLine(string.Format("Picture:\t\t{0}", customer.PictureUri.Value));
-			}
-			return sb.ToString();
-		}
-	}
+    public class GenerateCustomerInvoice
+    {
+        public string Create(Customer customer, string[] products)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("Invoice for #{0}", customer.AccountNumber));
+            sb.AppendLine();
+            sb.AppendLine(string.Format("To:\t\t{0} {1}", customer.FirstName, customer.LastName));
+            sb.AppendLine(string.Format("Street:\t\t{0}", customer.AddressStreet));
+            sb.AppendLine(string.Format("City:\t\t{0}", customer.AddressCity));
+            sb.AppendLine(string.Format("Country:\t\t{0}", customer.AddressCountry));
+            sb.AppendLine(customer.PictureUri.Match(
+                ifSome: url => string.Format("Picture:\t\t{0}", customer.PictureUri.Value),
+                ifNone: () => "No Picture"));
+            return sb.ToString();
+        }
+    }
 }
 
