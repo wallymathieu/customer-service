de56a1ea0f3e5441ff02bf25938024fe0ba6331b
Oskar Gewalli <oskar@gewalli.se>
2015-09-02 08:10:58 +0200

    hxt or hexpat or?

--- scotty/Main.hs
+++ scotty/Main.hs
@@ -3,6 +3,21 @@ import Web.Scotty
 import Web.Scotty.Internal.Types (ActionT)
 import CustomerService.Domain
 import CustomerService.CustomerService
+import CustomerService.Xml as X
+-- | Parse the request body as a XML object and return it. Raises an exception if parse is unsuccessful.
+xmlData :: (X.FromXML a, ScottyError e, MonadIO m) => ActionT e m a
+xmlData = do
+    b <- body
+    either (\e -> raise $ stringError $ "xmlData - no parse: " ++ e ++ ". Data was:" ++ BL.unpack b) return $ A.eitherDecode b
+
+-- | Set the body of the response to the JSON encoding of the given value. Also sets \"Content-Type\"
+-- header to \"application/json; charset=utf-8\" if it has not already been set.
+xml :: (X.ToXML a, ScottyError e, Monad m) => a -> ActionT e m ()
+xml v = do
+    changeHeader addIfNotPresent "Content-Type" "application/json; charset=utf-8"
+    raw $ A.encode v
+
+
 main = scotty 3000 $ do
     get "/CustomerService.svc/GetAllCustomers" $ do
         xml $ getAllCustomers 
