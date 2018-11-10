949534280aa72a4fc85568229c84eef586758239
Oskar Gewalli <oskar@gewalli.se>
2018-11-10 18:32:45 +0100

    Adding view instead of inline html

--- aspnet-mvc/CustomerService/Controllers/CustomerServiceController.cs
+++ aspnet-mvc/CustomerService/Controllers/CustomerServiceController.cs
@@ -3,28 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Customers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return Content( @"
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Customer service</title>
-    <meta charset=""utf-8"" />
-</head>
-<body>
-Copy of 
-<a href=""http://www.galasoft.ch/labs/Customers/CustomerService.svc"">galasoft CustomerService</a>
-used in order to demonstrate mvvm.
-You can mock implementation of the api here:
-  <a href=""/CustomerService.svc"">Customer service</a>.
-</body>
-</html>
-", "text/html; charset=utf-8");
-        }
-    }
     [Route("CustomerService.svc")]
     [ApiController]
     public class CustomerServiceController : ControllerBase
