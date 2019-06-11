bad1b0cee047e0c57ac546cee11ba2241a376436
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-06-11 21:21:00 +0200

    Remove soapcore submodule (#14)
    * - submodule
    
    * Using package reference

--- wcf/CustomerService/ICustomerService.cs
+++ wcf/CustomerService/ICustomerService.cs
@@ -6,15 +6,20 @@ namespace Customers
     [ServiceContract(ConfigurationName = "ICustomerService")]
     public interface ICustomerService
     {
-        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetCustomers", ReplyAction = "http://tempuri.org/ICustomerService/GetCustomersResponse")]
+        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetCustomers",
+            ReplyAction = "http://tempuri.org/ICustomerService/GetCustomersResponse")]
         Task<ArrayOfCustomer> GetCustomersAsync(string lastName);
-        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetCustomerByAccountNumber", ReplyAction = "http://tempuri.org/ICustomerService/GetCustomerByAccountNumberResponse")]
+        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetCustomerByAccountNumber", 
+            ReplyAction = "http://tempuri.org/ICustomerService/GetCustomerByAccountNumberResponse")]
         Task<Customer> GetCustomerByAccountNumberAsync(int accountNumber);
-        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetAllCustomers", ReplyAction = "http://tempuri.org/ICustomerService/GetAllCustomersResponse")]
+        [OperationContract(Action = "http://tempuri.org/ICustomerService/GetAllCustomers", 
+            ReplyAction = "http://tempuri.org/ICustomerService/GetAllCustomersResponse")]
         Task<ArrayOfCustomer> GetAllCustomersAsync();
-        [OperationContract(Action = "http://tempuri.org/ICustomerService/SaveCustomer", ReplyAction = "http://tempuri.org/ICustomerService/SaveCustomerResponse")]
+        [OperationContract(Action = "http://tempuri.org/ICustomerService/SaveCustomer", 
+            ReplyAction = "http://tempuri.org/ICustomerService/SaveCustomerResponse")]
         Task<bool> SaveCustomerAsync(Customer customer);
-        [OperationContract(Action = "http://tempuri.org/ICustomerService/SaveCustomerLastName", ReplyAction = "http://tempuri.org/ICustomerService/SaveCustomerLastNameResponse")]
+        [OperationContract(Action = "http://tempuri.org/ICustomerService/SaveCustomerLastName", 
+            ReplyAction = "http://tempuri.org/ICustomerService/SaveCustomerLastNameResponse")]
         Task<bool> SaveCustomerLastNameAsync(string accountNumber, string newName);
     }
 }
